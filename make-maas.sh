7d5d62e274db3d92d85a75a4d96578d60fc9ad7d
Chris Sanders <Sanders.Chris@gmail.com>
2017-11-03 19:39:20 -0500

    LXD network not available in stable lxc version

--- make-maas.sh
+++ make-maas.sh
@@ -2,9 +2,11 @@
 
 CONTAINER=$1
 
+# Find LXC version
 MAJOR=$(lxc version | awk -F. '{print $1}')
 MINOR=$(lxc version | awk -F. '{print $2}')
 
+# Create container
 echo "Creating container $CONTAINER"
 lxc profile create maas-profile 2> /dev/null
 lxc profile edit maas-profile < maas-profile
@@ -20,8 +22,16 @@ fi
 echo "Sleeping to wait for IP"
 sleep 5
 
+# Setup LXD forward for pxe requests
+# TODO: Make this work on 2.0.x stable LXC
 IPADDRESS=$(lxc info $CONTAINER | awk -F"[: \t]+" '/eth0:.*inet[^6]/ {print $4}')
-echo "Setting up pxe redirect for IP $IPADDRESS"
-lxc network set lxdbr0 raw.dnsmasq dhcp-boot=pxelinux.0,$CONTAINER,$IPADDRESS && systemctl restart lxd.service
+if [ $MAJOR -eq 2 ] && [ $MINOR -eq 0 ]
+then
+    echo "LXC Stable branch not scripted for dnsmasq settings PXE will not work"
+    echo "To install the backport: apt install -t xenial-backports lxc lxc-client"
+else
+    echo "Setting up pxe redirect for IP $IPADDRESS"
+    lxc network set lxdbr0 raw.dnsmasq dhcp-boot=pxelinux.0,$CONTAINER,$IPADDRESS && systemctl restart lxd.service
+fi
 
 echo "MAAS will become available at: http://$IPADDRESS/MAAS with user/password admin/admin"
