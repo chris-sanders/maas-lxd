f6bc7ff4a76cbf79753b8a86bcfb8f365204c9ed
Chris Sanders <sanders.chris@gmail.com>
2017-12-01 18:17:44 -0600

    Switch curtin and partitions for bluestore sizes/values

--- scripts/make_partitions.sh
+++ scripts/make_partitions.sh
@@ -21,17 +21,14 @@ maas admin partition delete $MACHINE sdb sdb-part1
 maas admin block-device set-boot-disk $MACHINE sdb
 maas admin partitions create $MACHINE sda size=5M
 maas admin partitions create $MACHINE sda size=100G
-maas admin partitions create $MACHINE sda size=1G
+maas admin partitions create $MACHINE sda size=100M
 # maas admin partitions create $MACHINE sda size=XXX-Manual
 maas admin block-device update $MACHINE sda partition_table_type=gpt
-#maas admin partition format $MACHINE sda sda-part2 fstype=ext4
-#maas admin partition mount $MACHINE sda sda-part2 mount_point=/
 
 # Setup sdb
 maas admin block-device set-boot-disk $MACHINE sda
 maas admin partitions create $MACHINE sdb size=5M
 maas admin partitions create $MACHINE sdb size=100G
-maas admin partitions create $MACHINE sdb size=1G
+maas admin partitions create $MACHINE sdb size=100M
 # maas admin partitions create $MACHINE sdb size=XXX-Manual
 maas admin block-device update $MACHINE sdb partition_table_type=gpt
-# maas admin partition format $MACHINE sdb sdb-part2 fstype=ext4
