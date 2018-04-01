c194b888abe265199e5bf3396ae8b6b6dc1979b5
Chris Sanders <sanders.chris@gmail.com>
2018-03-31 22:29:46 -0500

    Added some recommened settings to maas-profile

--- scripts/make_partitions.sh
+++ scripts/make_partitions.sh
@@ -4,26 +4,26 @@
 
 MACHINE=$1
 # # Clear the partitions
-for partid in $(maas maas-root partitions read $MACHINE sda |jq '.[] | .id'); do
-  maas maas-root partition delete $MACHINE sda $partid
+for partid in $(maas admin partitions read $MACHINE sda |jq '.[] | .id'); do
+  maas admin partition delete $MACHINE sda $partid
 done
 
-for partid in $(maas maas-root partitions read $MACHINE sdb |jq '.[] | .id'); do
-  maas maas-root partition delete $MACHINE sdb $partid
+for partid in $(maas admin partitions read $MACHINE sdb |jq '.[] | .id'); do
+  maas admin partition delete $MACHINE sdb $partid
 done
 
 # https://bugs.launchpad.net/maas/+bug/1712505
-maas maas-root block-device set-boot-disk $MACHINE sdc
-maas maas-root partitions create $MACHINE sdc size=5M
+maas admin block-device set-boot-disk $MACHINE sdc
+maas admin partitions create $MACHINE sdc size=5M
 
 # Setup sda
-# maas maas-root block-device set-boot-disk $MACHINE sdb
-maas maas-root partitions create $MACHINE sda size=5M
-maas maas-root partitions create $MACHINE sda size=200G
-# maas maas-root block-device update $MACHINE sda partition_table_type=gpt
+# maas admin block-device set-boot-disk $MACHINE sdb
+maas admin partitions create $MACHINE sda size=5M
+maas admin partitions create $MACHINE sda size=500G
+# maas admin block-device update $MACHINE sda partition_table_type=gpt
 
 # Setup sdb
-# maas maas-root block-device set-boot-disk $MACHINE sda
-maas maas-root partitions create $MACHINE sdb size=5M
-maas maas-root partitions create $MACHINE sdb size=200G
-# maas maas-root block-device update $MACHINE sdb partition_table_type=gpt
+# maas admin block-device set-boot-disk $MACHINE sda
+maas admin partitions create $MACHINE sdb size=5M
+maas admin partitions create $MACHINE sdb size=500G
+# maas admin block-device update $MACHINE sdb partition_table_type=gpt
