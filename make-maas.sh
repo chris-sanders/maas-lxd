d5ec11e6d364c28d24204e36fd3481afae15125f
dmzoneill <david.o.neill@canonical.com>
2019-01-11 13:10:02 +0000

    Updates for LXC 3

--- make-maas.sh
+++ make-maas.sh
@@ -2,17 +2,37 @@
 
 CONTAINER=$1
 
-# Find LXC version
-MAJOR=$(lxc version | awk -F. '{print $1}')
-MINOR=$(lxc version | awk -F. '{print $2}')
+lxc version | grep "Server version" >/dev/null 2>&1
+
+# lxc version > 3 has dififerent output
+if [ $? -eq 0 ]
+then
+    # Find Major/Minor LXC version 3
+    VER=$(lxc version | grep Server | awk -F: '{print $2}'i | sed 's/\s\+//g')
+    MAJOR=$(echo $VER | awk -F. '{print $1}')
+    MINOR=$(echo $VER | awk -F. '{print $2}')
+else
+    # Find Major/Minor < LXC version 3
+    MAJOR=$(lxc version | awk -F. '{print $1}')
+    MINOR=$(lxc version | awk -F. '{print $2}')
+fi
+
+if [ $MAJOR -eq 3 ]
+then
+    sed 's/lxc.aa_profile/lxc.apparmor.profile/g' -i maas-profile
+fi
 
 # Create container
 echo "Creating container $CONTAINER"
 lxc profile create maas-profile 2> /dev/null
 lxc profile edit maas-profile < maas-profile
-if [ $MAJOR -eq 2 ] && [ $MINOR -eq 0 ]
+if [ $MAJOR -eq 3 ]
 then
-    lxc launch ubuntu:16.04 $CONTAINER -p maas-profile
+    # v3 expects pool name, use default
+    lxc launch ubuntu:16.04 $CONTAINER -p maas-profile -s default
+elif [ $MAJOR -eq 2 && $MINOR -eq 0 ]
+then
+    lxc launch ubuntu:16.04 $CONTAINER -p maas-profile 
 else
     lxc profile create root-device 2> /dev/null
     lxc profile edit root-device < root-device
@@ -25,7 +45,7 @@ sleep 10
 # Setup LXD forward for pxe requests
 # TODO: Make this work on 2.0.x stable LXC
 IPADDRESS=$(lxc info $CONTAINER | awk -F"[: \t]+" '/eth0:.*inet[^6]/ {print $4}')
-if [ $MAJOR -eq 2 ] && [ $MINOR -eq 0 ]
+if [[( $MAJOR -eq 2 || $MAJOR -eq 3 ) && $MINOR -eq 0 ]]
 then
     echo "LXC Stable branch not scripted for dnsmasq settings PXE will not work"
     echo "To install the backport: apt install -t xenial-backports lxc lxc-client"
